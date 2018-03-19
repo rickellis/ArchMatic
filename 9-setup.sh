3a4745106e49926aab2ee12b3df0dcfc0d669ae8
rickellis <rickellis@gmail.com>
2018-03-19 16:03:30 +0000

    removed sed -e flag

--- 9-setup.sh
+++ 9-setup.sh
@@ -41,7 +41,7 @@ EOF
 
 echo
 echo "Updating /bin/startx"
-sudo sed -i -e 's|xserverauthfile=\$HOME/.serverauth.\$\$|xserverauthfile=\$XAUTHORITY|g' /bin/startx
+sudo sed -i 's|xserverauthfile=\$HOME/.serverauth.\$\$|xserverauthfile=\$XAUTHORITY|g' /bin/startx
 
 # ------------------------------------------------------------------------
 
@@ -52,9 +52,9 @@ echo "Configuring LTS Kernel as a secondary boot option"
 sudo cp /boot/loader/entries/arch.conf /boot/loader/entries/arch-lts.conf
 
 # Swap various values
-sudo sed -i -e 's|Arch Linux|Arch Linux LTS Kernel|g' /boot/loader/entries/arch-lts.conf
-sudo sed -i -e 's|vmlinuz-linux|vmlinuz-linux-lts|g' /boot/loader/entries/arch-lts.conf
-sudo sed -i -e 's|initramfs-linux.img|initramfs-linux-lts.img|g' /boot/loader/entries/arch-lts.conf
+sudo sed -i 's|Arch Linux|Arch Linux LTS Kernel|g' /boot/loader/entries/arch-lts.conf
+sudo sed -i 's|vmlinuz-linux|vmlinuz-linux-lts|g' /boot/loader/entries/arch-lts.conf
+sudo sed -i 's|initramfs-linux.img|initramfs-linux-lts.img|g' /boot/loader/entries/arch-lts.conf
 
 # ------------------------------------------------------------------------
 
@@ -99,14 +99,15 @@ echo "Disabling Pulse .esd_auth module"
 # Pulse audio loads the `esound-protocol` module, which best I can tell is rarely needed.
 # That module creates a file called `.esd_auth` in the home directory which I'd prefer to not be there. So...
 
-sudo sed -i -e 's|load-module module-esound-protocol-unix|#load-module module-esound-protocol-unix|g' /etc/pulse/default.pa
+sudo sed -i 's|load-module module-esound-protocol-unix|#load-module module-esound-protocol-unix|g' /etc/pulse/default.pa
 
 # ------------------------------------------------------------------------
 
 echo
 echo "Enabling bluetooth auto-start"
 
-sudo sed -i 's/'#AutoEnable=false'/'AutoEnable=true'/g' /etc/bluetooth/main.conf
+sudo sed -i 's|#AutoEnable=false|AutoEnable=true|g' /etc/bluetooth/main.conf
+
 
 echo
 echo "Starting systemctl bluetooth service"

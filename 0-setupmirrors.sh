4025eb3235691b46275a4e92b05a0652eda0ee25
Chris Titus <dfm.titus@gmail.com>
2018-12-20 14:21:20 -0600

    Update 0-setupmirrors.sh

--- 0-setupmirrors.sh
+++ 0-setupmirrors.sh
@@ -1,2 +1,3 @@
+pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist

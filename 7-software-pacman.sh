03ec7e7d6dd29e5e6ed16aa6a46ae42576adc8ad
Chris Titus <dfm.titus@gmail.com>
2019-09-14 13:35:09 -0500

    Update 7-software-pacman.sh

--- 7-software-pacman.sh
+++ 7-software-pacman.sh
@@ -20,21 +20,14 @@ PKGS=(
     # TERMINAL UTILITIES --------------------------------------------------
 
     'bash-completion'       # Tab completion for Bash
-    'bc'                    # Precision calculator language
     'bleachbit'             # File deletion utility
     'cronie'                # cron jobs
     'curl'                  # Remote content retrieval
-    'elinks'                # Terminal based web browser
-    'feh'                   # Terminal-based image viewer/manipulator
     'file-roller'           # Archive utility
-    'gnome-keyring'         # System password storage
     'gtop'                  # System monitoring via terminal
     'gufw'                  # Firewall manager
     'hardinfo'              # Hardware info app
     'htop'                  # Process viewer
-    'inxi'                  # System information utility
-    'jq'                    # JSON parsing library
-    'jshon'                 # JSON parsing library
     'neofetch'              # Shows system info when you launch terminal
     'ntp'                   # Network Time Protocol to set time via network.
     'numlockx'              # Turns on numlock in X11
@@ -46,7 +39,7 @@ PKGS=(
     'unrar'                 # RAR compression program
     'unzip'                 # Zip compression program
     'wget'                  # Remote content retrieval
-    'xfce4-terminal'        # Terminal emulator
+    'terminator'            # Terminal emulator
     'zenity'                # Display graphical dialog boxes via shell scripts
     'zip'                   # Zip compression program
     'zsh'                   # ZSH shell
@@ -57,7 +50,6 @@ PKGS=(
     'autofs'                # Auto-mounter
     'exfat-utils'           # Mount exFat drives
     'gparted'               # Disk utility
-    'gnome-disks'           # Disk utility
     'ntfs-3g'               # Open source implementation of NTFS file system
     'parted'                # Disk utility
 
@@ -65,7 +57,7 @@ PKGS=(
 
     'catfish'               # Filesystem search
     'conky'                 # System information viewer
-    'nemo'                  # Filesystem browser
+    'nautilus'              # Filesystem browser
     'veracrypt'             # Disc encryption utility
     'variety'               # Wallpaper changer
     'xfburn'                # CD burning application
@@ -73,21 +65,16 @@ PKGS=(
 
     # DEVELOPMENT ---------------------------------------------------------
 
-    'atom'                  # Text editor
-    'apache'                # Apache web server
+    'kate'                  # Text editor
     'clang'                 # C Lang compiler
     'cmake'                 # Cross-platform open-source make system
     'electron'              # Cross-platform development using Javascript
     'git'                   # Version control system
     'gcc'                   # C/C++ compiler
     'glibc'                 # C libraries
-    'mariadb'               # Drop-in replacement for MySQL
     'meld'                  # File/directory comparison
     'nodejs'                # Javascript runtime environment
     'npm'                   # Node package manager
-    'php'                   # Web application scripting language
-    'php-apache'            # Apache PHP driver
-    'postfix'               # SMTP mail server
     'python'                # Scripting language
     'qtcreator'             # C++ cross platform IDE
     'qt5-examples'          # Project demos for Qt
@@ -95,40 +82,29 @@ PKGS=(
 
     # WEB TOOLS -----------------------------------------------------------
 
-    'firefox'               # Web browser
     'filezilla'             # FTP Client
-    'flashplugin'           # Flash
-
+    
     # COMMUNICATIONS ------------------------------------------------------
 
     'hexchat'               # Multi format chat
-    'irssi'                 # Terminal based IIRC
-
+    
     # MEDIA ---------------------------------------------------------------
 
-    'kdenlive'                 # Movie Render
-    'lollypop'              # Music player
+    'kdenlive'              # Movie Render
     'obs-studio'            # Record your screen
     'vlc'                   # Video player
-    'xfce4-screenshooter'   # Screen capture.
-
+    
     # GRAPHICS AND DESIGN -------------------------------------------------
 
     'gcolor2'               # Colorpicker
     'gimp'                  # GNU Image Manipulation Program
-    'inkscape'              # Vector image creation app
-    'imagemagick'           # Command line image manipulation tool
-    'nomacs'                # Image viewer
-    'pngcrush'              # Tools for optimizing PNG images
     'ristretto'             # Multi image viewer
 
     # PRODUCTIVITY --------------------------------------------------------
 
-    'galculator'            # Gnome calculator
     'hunspell'              # Spellcheck libraries
     'hunspell-en'           # English spellcheck library
     'libreoffice-fresh'     # Libre office with extra features
-    'mousepad'              # XFCE simple text editor
     'xpdf'                  # PDF viewer
 
     # VIRTUALIZATION ------------------------------------------------------

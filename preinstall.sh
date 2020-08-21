eb53845503e6b1dc99c22379b6d8d7572f607efb
Liam Powell <60463234+gfelipe099@users.noreply.github.com>
2020-08-21 17:20:35 +0100

    Added Network Setup

--- preinstall.sh
+++ preinstall.sh
@@ -78,6 +78,12 @@ options root=${DISK}1 rw
 EOF
 
 echo "--------------------------------------"
+echo "--          Network Setup           --"
+echo "--------------------------------------"
+pacman -S network-manager dhclient --noconfirm --needed
+systemctl enable --now NetworkManager
+
+echo "--------------------------------------"
 echo "--      Set Password for Root       --"
 echo "--------------------------------------"
 echo "Enter password for root user: "

14ec4ab4ede9b53b4bc7647bec9ab2c3f47f75e1
Cameron Youell <cameronyouell@gmail.com>
2021-10-15 15:10:33 +1100

    Hopefully auto get iso

--- preinstall.sh
+++ preinstall.sh
@@ -9,13 +9,18 @@
 (
 
 echo "-------------------------------------------------"
+echo "-       Getting ISO code from ifconfig.co       -"
+echo "-------------------------------------------------"
+iso=$(curl -4 ifconfig.co/country-iso)
+
+echo "-------------------------------------------------"
 echo "Setting up mirrors for optimal download - US Only"
 echo "-------------------------------------------------"
 timedatectl set-ntp true
 pacman -S --noconfirm pacman-contrib
 pacman -S --noconfirm reflector rsync
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-reflector -a 48 -c us -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 mkdir /mnt
 
 

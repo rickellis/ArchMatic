abe7efa1fc2ba3548aa14b3a805c4ce749922c69
Chris Titus <dfm.titus@gmail.com>
2021-10-18 09:35:40 -0500

    Create kderice-restore.sh

---
+++ kderice-restore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -r $HOME/ArchMatic/KDErice/.config $HOME/.config/
+sudo cp -r /etc/sddm.conf.d/ $HOME/ArchMatic/KDErice/sddm.conf.d
+cp -r $HOME/ArchMatic/KDErice/.kde4/share/config $HOME/.kde4/share/config/
+cp -r $HOME/ArchMatic/KDErice/.local/share/dolphin $HOME/.local/share/dolphin
+cp -r $HOME/ArchMatic/KDErice/.local/share/plasma $HOME/.local/share/plasma

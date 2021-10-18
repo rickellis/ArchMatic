85963c49b7be7dec80162de1db9357bdfa4022da
Chris Titus <dfm.titus@gmail.com>
2021-10-18 09:34:13 -0500

    ricebackup

---
+++ kderice-backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -r $HOME/.config/ $HOME/ArchMatic/KDErice/.config
+sudo cp -r /etc/sddm.conf.d/ $HOME/ArchMatic/KDErice/sddm.conf.d
+cp -r $HOME/.kde4/share/config/ $HOME/ArchMatic/KDErice/.kde4/share/config
+cp -r $HOME/.local/share/dolphin $HOME/ArchMatic/KDErice/.local/share/dolphin
+cp -r $HOME/.local/share/plasma $HOME/ArchMatic/KDErice/.local/share/plasma
--- kderice.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp -r $HOME/.config/ $HOME/ArchMatic/KDErice
-sudo cp -r /etc/sddm.conf.d/ $HOME/ArchMatic/KDErice
-cp -r $HOME/.kde4/share/config/ $HOME/ArchMatic/KDErice
-cp -r $HOME/.local/share/dolphin $HOME/ArchMatic/KDErice
-cp -r $HOME/.local/share/kwin $HOME/ArchMatic/KDErice
-cp -r $HOME/.local/share/plasma $HOME/ArchMatic/KDErice

991c872fd4cfb1a3e6a2ff7f7e1b5f59dd710646
Chris Titus <dfm.titus@gmail.com>
2021-10-18 09:30:11 -0500

    Update kderice.sh

--- kderice.sh
+++ kderice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp -r $HOME/.config/ $HOME/ArchMatic/KDErice
-sudo cp /etc/sddm.conf.d/ $HOME/ArchMatic/KDErice
+sudo cp -r /etc/sddm.conf.d/ $HOME/ArchMatic/KDErice
 cp -r $HOME/.kde4/share/config/ $HOME/ArchMatic/KDErice
 cp -r $HOME/.local/share/dolphin $HOME/ArchMatic/KDErice
 cp -r $HOME/.local/share/kwin $HOME/ArchMatic/KDErice

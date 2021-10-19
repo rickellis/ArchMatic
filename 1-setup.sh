572f78385604d63617b18e0e01c8354b4c1971d1
Chris Titus <dfm.titus@gmail.com>
2021-10-19 11:47:26 -0500

    bugfix

--- 1-setup.sh
+++ 1-setup.sh
@@ -293,7 +293,7 @@ EOF
     pacman -S nvidia-dkms dkms --noconfirm --needed
 elif lspci | grep -E "Radeon"; then
     pacman -S xf86-video-amdgpu --noconfirm --needed
-else
+elif lspci | grep -E "Integrated Graphics Controller"; then
     pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
 fi
 echo -e "\nDone!\n"

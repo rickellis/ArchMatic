7249db02ca7b13b20868cbcd826c19fa8569a301
Chris Titus <dfm.titus@gmail.com>
2021-10-18 22:52:26 -0500

    Update kderice-restore.sh

--- kderice-restore.sh
+++ kderice-restore.sh
@@ -4,4 +4,5 @@ export PATH=$PATH:~/.local/bin
 cp -r $HOME/ArchMatic/dotfiles/ $HOME/.config/
 pip install konsave
 konsave -i $HOME/ArchMatic/kde.knsv
-konsave -a $HOME/ArchMatic/kde
+sleep 1
+konsave -a kde

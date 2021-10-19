ae17c9ce3f80b966ca6e6ea2c65c77df6add60ee
Chris Titus <dfm.titus@gmail.com>
2021-10-18 22:09:37 -0500

    Update archmatic.sh

--- archmatic.sh
+++ archmatic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
     bash 0-preinstall.sh
-    arch-chroot /mnt/ -c /root/ArchMatic/1-setup.sh
+    arch-chroot /mnt /root/ArchMatic/1-setup.sh
     source /mnt/root/ArchMatic/install.conf
 	arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchMatic/2-user.sh
-    archchroot 3-post-setup.sh
+    arch-chroot /mnt /root/ArchMatic/3-post-setup.sh
